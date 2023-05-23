823c6366d227aa9a82b397d0ae0b5e3fb3cf3ec2
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2023-05-23 17:57:45 +0200

    UI

---
+++ Hot Coffee (MVVM)/Contrller/OrdersTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrdersTableViewController.swift
+//  Hot Coffee (MVVM)
+//
+//  Created by Youssef Eldeeb on 22/05/2023.
+//
+
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
